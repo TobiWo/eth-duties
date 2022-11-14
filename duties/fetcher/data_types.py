3d3e55b1187375cf93a0d0a2edefc1a16511a6ee
towo <Tobias.Wohland@t-systems.com>
2022-11-14 19:31:54 +0100

    feat(duty-fetcher): Restructure duty fetcher and clean the code
    * Delete RawValidatorDuty in favor for one data class
    * Add constants module
    * Improve error handling in fetcher module

--- duties/fetcher/data_types.py
+++ duties/fetcher/data_types.py
@@ -1,5 +1,5 @@
 """
-Defines different dataclasses
+Defines different data types
 """
 
 from dataclasses import dataclass
@@ -7,20 +7,22 @@ from enum import Enum
 from dataclass_wizard import JSONWizard
 
 
-@dataclass
-class RawValidatorDuty(JSONWizard):
-    pubkey: str
-    validator_index: int
-    slot: int
-
-
 class DutyType(Enum):
+    """
+    Defines a validator duty type
+    """
+
     ATTESTATION = 1
     PROPOSING = 2
 
 
 @dataclass
-class ValidatorDuty:
+class ValidatorDuty(JSONWizard):
+    """
+    Validator duty relevant data points
+    """
+
+    pubkey: str
     validator_index: int
-    target_slot: int
-    type: DutyType
+    slot: int
+    type: DutyType = DutyType.ATTESTATION
