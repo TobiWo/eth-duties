9314116531937930e69609403e79bdb55deee29e
TobiWo <Emailkeeper@gmx.de>
2022-11-24 14:24:37 +0100

    refactor(constants): Move constants to separate package with separated modules

---
+++ duties/fetcher/constants/endpoints.py
@@ -0,0 +1,6 @@
+"""Defines api endpoints
+"""
+
+ATTESTATION_DUTY_ENDPOINT = "/eth/v1/validator/duties/attester/"
+BLOCK_PROPOSING_DUTY_ENDPOINT = "/eth/v1/validator/duties/proposer/"
+BEACON_GENESIS_ENDPOINT = "/eth/v1/beacon/genesis"
