67e8ea7db304bd86038059cfbdcaf0efb5c40734
TobiWo <Emailkeeper@gmx.de>
2022-11-29 19:20:10 +0100

    feat(printer): Add logic to create sync committee duty related message

--- duties/fetcher/printer.py
+++ duties/fetcher/printer.py
@@ -6,11 +6,12 @@
 from time import time, strftime, gmtime
 from logging import getLogger
 from typing import List
+from math import ceil, trunc
 from colorama import Back, Style
 from fetcher.data_types import ValidatorDuty, DutyType
-from fetcher.constants import ethereum
 from fetcher.constants import logging
 from fetcher.constants import program
+from protocol import protocol
 
 
 def print_time_to_next_duties(
@@ -27,8 +28,10 @@ def print_time_to_next_duties(
     if validator_duties:
         for index, duty in enumerate(validator_duties):
             now = time()
-            seconds_to_next_duty = duty.slot * ethereum.SLOT_TIME + genesis_time - now
-            logging_message = __create_logging_message(seconds_to_next_duty, duty)
+            seconds_to_next_duty = duty.slot * protocol.SLOT_TIME + genesis_time - now
+            logging_message = __create_logging_message(
+                seconds_to_next_duty, duty, genesis_time
+            )
             if index == len(validator_duties) - 1:
                 logging_message += "\n"
             logger.info(logging_message)
@@ -36,7 +39,9 @@ def print_time_to_next_duties(
         logger.info(logging.NO_UPCOMING_DUTIES_MESSAGE)
 
 
-def __create_logging_message(seconds_to_next_duty: float, duty: ValidatorDuty) -> str:
+def __create_logging_message(
+    seconds_to_next_duty: float, duty: ValidatorDuty, genesis_time: int
+) -> str:
     """Creates the logging message for the provided duty
 
     Args:
@@ -46,7 +51,9 @@ def __create_logging_message(seconds_to_next_duty: float, duty: ValidatorDuty) -
     Returns:
         str: Message which will be logged to stdout
     """
-    if seconds_to_next_duty < 0:
+    if duty.type is DutyType.SYNC_COMMITTEE:
+        logging_message = __create_sync_committee_logging_message(genesis_time, duty)
+    elif seconds_to_next_duty < 0:
         logging_message = (
             f"Upcoming {duty.type.name} duty "
             f"for validator {duty.validator_index} outdated. "
@@ -60,6 +67,35 @@ def __create_logging_message(seconds_to_next_duty: float, duty: ValidatorDuty) -
             f"{__get_printing_color(seconds_to_next_duty, duty)}"
             f"Validator {duty.validator_index} has next {duty.type.name} duty in: "
             f"{time_to_next_duty} min. (slot: {duty.slot}){Style.RESET_ALL}"
+        )
+    return logging_message
+
+
+def __create_sync_committee_logging_message(
+    genesis_time: int, duty: ValidatorDuty
+) -> str:
+    current_epoch = protocol.get_current_epoch(genesis_time)
+    current_sync_committee_epoch_lower_bound: int = (
+        trunc(current_epoch / protocol.EPOCHS_PER_SYNC_COMMITTEE)
+        * protocol.EPOCHS_PER_SYNC_COMMITTEE
+    )
+    current_sync_committee_epoch_upper_bound: int = (
+        ceil(current_epoch / protocol.EPOCHS_PER_SYNC_COMMITTEE)
+        * protocol.EPOCHS_PER_SYNC_COMMITTEE
+    ) - 1
+    if duty.epoch in range(
+        current_sync_committee_epoch_lower_bound,
+        current_sync_committee_epoch_upper_bound,
+    ):
+        logging_message = (
+            f"{Back.RED}Validator {duty.validator_index} is in current sync committee "
+            f"(next sync committee starts at epoch "
+            f"{current_sync_committee_epoch_upper_bound + 1}){Style.RESET_ALL}"
+        )
+    else:
+        logging_message = (
+            f"{Back.YELLOW}Validator {duty.validator_index} will be in sync committee "
+            f"starting at epoch {current_sync_committee_epoch_upper_bound + 1}{Style.RESET_ALL}"
         )
     return logging_message
 
