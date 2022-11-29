301b4caa008cc999e20b9ac592eb5c9888e8f217
TobiWo <Emailkeeper@gmx.de>
2022-11-29 19:14:04 +0100

    feat(endpoints): Add sync duties endpoint

--- duties/fetcher/constants/endpoints.py
+++ duties/fetcher/constants/endpoints.py
@@ -2,5 +2,6 @@
 """
 
 ATTESTATION_DUTY_ENDPOINT = "/eth/v1/validator/duties/attester/"
+SYNC_COMMITTEE_DUTY_ENDPOINT = "/eth/v1/validator/duties/sync/"
 BLOCK_PROPOSING_DUTY_ENDPOINT = "/eth/v1/validator/duties/proposer/"
 BEACON_GENESIS_ENDPOINT = "/eth/v1/beacon/genesis"
