5016dcdb562b0523b5566c3413844d24877a0ae1
TobiWo <Emailkeeper@gmx.de>
2022-12-20 19:14:51 +0100

    feat(constants): Add new logging message for max number of supported validators

--- duties/constants/logging.py
+++ duties/constants/logging.py
@@ -10,3 +10,8 @@ NO_DATA_FIELD_IN_RESPONS_JSON_ERROR_MESSAGE = (
 SYSTEM_EXIT_MESSAGE = "Detected user intervention (SIGINT). Shutting down."
 NEXT_INTERVAL_MESSAGE = "Printing next interval..."
 NO_UPCOMING_DUTIES_MESSAGE = "No upcoming duties detected!"
+TOO_MANY_PROVIDED_VALIDATORS_MESSAGE = (
+    "Provided number of validator indices is higher than 300. "
+    "This surpasses the current maximum for fetching attestation and sync committee duties. "
+    "Checking for those duties will be skipped!"
+)
