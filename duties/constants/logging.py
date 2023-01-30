7d9536ee2e31f81d080d2795595c796ef719a8ca
TobiWo <Emailkeeper@gmx.de>
2023-01-30 16:06:54 +0100

    feat(constant): Update logging messages
    * Add new logging message for inactive validators
    * Refactor interval logging message
    * Add new logging message for possible waiting period

--- duties/constants/logging.py
+++ duties/constants/logging.py
@@ -1,6 +1,8 @@
 """Defines logging messages
 """
 
+from colorama import Back, Style
+
 CONNECTION_ERROR_MESSAGE = "Couldn't connect to beacon client. Retry in 2 second."
 READ_TIMEOUT_ERROR_MESSAGE = "Couldn't read from beacon client. Retry in 5 seconds."
 NO_RESPONSE_ERROR_MESSAGE = "Couldn't fetch any data from the beacon client."
@@ -8,10 +10,18 @@ NO_DATA_FIELD_IN_RESPONS_JSON_ERROR_MESSAGE = (
     "Response object does not include a 'data' field"
 )
 SYSTEM_EXIT_MESSAGE = "Detected user intervention (SIGINT). Shutting down."
-NEXT_INTERVAL_MESSAGE = "Printing next interval..."
+NEXT_INTERVAL_MESSAGE = "Logging next interval..."
 NO_UPCOMING_DUTIES_MESSAGE = "No upcoming duties detected!"
 TOO_MANY_PROVIDED_VALIDATORS_MESSAGE = (
     "Provided number of validator indices is higher than 300. "
     "This surpasses the current maximum for fetching attestation and sync committee duties. "
     "Checking for those duties will be skipped!"
+)
+HIGHER_PROCESSING_TIME_INFO_MESSAGE = (
+    "You provided %s validators. Fetching all necessary data may take some time."
+)
+INACTIVE_VALIDATORS_MESSAGE = (
+    f"{Back.YELLOW}The following provided validators are not active "
+    "and therefore will be skipped for further processing: %s"
+    f"{Style.RESET_ALL}"
 )
