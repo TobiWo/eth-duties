2f05725be36bd6748f59ce6a4f377aa324ecc3d0
TobiWo <Emailkeeper@gmx.de>
2023-06-15 10:09:08 +0200

    refactor(constants): Move graceful terminator initialization to main entrypoint

--- duties/constants/program.py
+++ duties/constants/program.py
@@ -1,19 +1,11 @@
 """Defines program related constants
 """
 
-from math import floor
-
-from cli.arguments import ARGUMENTS
-from helper.terminate import GracefulTerminator
-
 REQUEST_TIMEOUT = (3, 5)
 REQUEST_CONNECTION_ERROR_WAITING_TIME = 2
 REQUEST_READ_TIMEOUT_ERROR_WAITING_TIME = 5
 REQUEST_HEADER = {"Content-type": "application/json", "Accept": "application/json"}
 PRINTER_TIME_FORMAT = "%M:%S"
-GRACEFUL_TERMINATOR = GracefulTerminator(
-    floor(ARGUMENTS.mode_cicd_waiting_time / ARGUMENTS.interval)
-)
 THRESHOLD_TO_INFORM_USER_FOR_WAITING_PERIOD = 5000
 NOT_ALLOWED_CHARACTERS_FOR_VALIDATOR_PARSING = [".", ","]
 NUMBER_OF_VALIDATORS_PER_REST_CALL = 1000
