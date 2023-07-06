014de604b2fbb8295ad0a6ac56736cbead044d6b
TobiWo <Emailkeeper@gmx.de>
2023-07-06 10:16:13 +0200

    feat(cli): Add rest api relevant cli flag * Add no-log mode

--- duties/cli/types.py
+++ duties/cli/types.py
@@ -8,6 +8,7 @@ class Mode(Enum):
     """Defines the mode at which eth-duties will run"""
 
     LOG = "log"
+    NO_LOG = "no-log"
     CICD_EXIT = "cicd-exit"
     CICD_WAIT = "cicd-wait"
     CICD_FORCE_GRACEFUL_EXIT = "cicd-force-graceful-exit"
