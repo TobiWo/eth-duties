511cb3be79cf709e93e888dfba2f568059756f63
TobiWo <Emailkeeper@gmx.de>
2023-07-13 15:19:07 +0200

    feat(rest): Update rest starting module

--- duties/rest/app.py
+++ duties/rest/app.py
@@ -2,15 +2,24 @@
 """
 
 from cli.arguments import ARGUMENTS
+from fastapi import FastAPI
 from fetcher import get_logging_config
-from rest.server import RestServer
+from rest.core.server import RestServer
+from rest.router.main import router
 from uvicorn import Config as UvicornConfig
+
+app = FastAPI(
+    title="eth-duties REST API",
+    description="REST endpoints exposed via eth-duties",
+    version="v0.4.0",
+)
+app.include_router(router)
 
 
 def start_rest_server() -> None:
     """Starts the rest server with come configuration"""
     config = UvicornConfig(
-        "rest.endpoints:app",
+        "rest.app:app",
         host="127.0.0.1",
         port=5000,
         log_config=get_logging_config(ARGUMENTS.log),
