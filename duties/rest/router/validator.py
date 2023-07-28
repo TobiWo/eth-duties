6f81a6b96c2c7c884f93deea9cf42423ecd81bf3
TobiWo <Emailkeeper@gmx.de>
2023-07-28 13:02:13 +0200

    feat(rest): Update rest modules for shared memory usage

--- duties/rest/router/validator.py
+++ duties/rest/router/validator.py
@@ -14,13 +14,29 @@ validator_router = APIRouter(prefix="/validator", tags=["validator"])
 async def add_validator_identifier(
     validator_identifiers: List[str], request: Request
 ) -> List[ValidatorIdentifier]:
-    """Adds validator identifier to the running eth-duties instance (only in memory)"""
-    return await update_validator_identifiers(request.method, validator_identifiers)
+    """Add validator identifiers to the running eth-duties instance (only in memory)_summary_
+
+    Args:
+        validator_identifiers (List[str]): Provided validator identifiers
+        request (Request): Sent request
+
+    Returns:
+        List[ValidatorIdentifier]: Provided validator identifiers
+    """
+    response = await update_validator_identifiers(validator_identifiers, request.method)
+    if response:
+        return response
+    return []
 
 
 @validator_router.delete("/identifier", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_validator_identifier(
     validator_identifier: List[str], request: Request
 ) -> None:
-    """Adds validator identifier to the running eth-duties instance (only in memory)"""
-    await update_validator_identifiers(request.method, validator_identifier)
+    """Delete validator identifiers from the running eth-duties instance (only in memory)
+
+    Args:
+        validator_identifier (List[str]): Provided validator identifiers
+        request (Request): Sent request
+    """
+    await update_validator_identifiers(validator_identifier, request.method)
--- duties/rest/service/validator.py
+++ duties/rest/service/validator.py
@@ -2,31 +2,53 @@
 """
 
 from logging import getLogger
-from typing import List
+from typing import Dict, List
 
 from fetcher.data_types import ValidatorIdentifier
-from fetcher.parser.validators import (
-    update_validator_identifiers as update_global_validator_identifiers,
-)
-from fetcher.parser.validators import write_updated_validator_identifiers_to_disk
+from fetcher.identifier import core
+from fetcher.identifier.parser import update_shared_active_validator_identifiers
+from rest.core.types import HttpMethod
 
 __LOGGER = getLogger()
 
 
 async def update_validator_identifiers(
-    http_method: str, validator_identifiers: List[str]
-) -> List[ValidatorIdentifier]:
-    """Updates validator identifiers
+    provided_validator_identifiers: List[str], http_method: str
+) -> List[ValidatorIdentifier] | None:
+    """Update validator identifiers in the running eth-duties instance
 
     Args:
-        http_method (str): Used http method
-        validator_identifier (List[str]): Provided validator identifiers
+        provided_validator_identifiers (List[str]): Provides validator identifiers
+        http_method (str): Request method
+
+    Returns:
+        List[ValidatorIdentifier] | None: Response
     """
-    response = await update_global_validator_identifiers(
-        http_method, validator_identifiers
+    provided_raw_validator_identifiers = __create_raw_validator_identifiers_from_list(
+        provided_validator_identifiers
     )
-    await write_updated_validator_identifiers_to_disk()
-    __LOGGER.info("%s validator identifiers: %s", http_method, validator_identifiers)
-    if response:
-        return list(response.values())
-    return [ValidatorIdentifier()]
+    __LOGGER.info(
+        "%s validator identifiers: %s", http_method, provided_validator_identifiers
+    )
+    await update_shared_active_validator_identifiers(
+        provided_raw_validator_identifiers, http_method
+    )
+    if http_method == HttpMethod.POST.value:
+        return list(provided_raw_validator_identifiers.values())
+    return None
+
+
+def __create_raw_validator_identifiers_from_list(
+    provided_validator_identifiers: List[str],
+) -> Dict[str, ValidatorIdentifier]:
+    """Parse validator identifiers provided by the user into a identifier dict
+
+    Returns:
+        Dict[str, ValidatorIdentifier]: Raw validator identifiers as provided by the user
+    """
+    return {
+        core.get_validator_index_or_pubkey(
+            None, core.create_raw_validator_identifier(str(validator))
+        ): core.create_raw_validator_identifier(str(validator))
+        for validator in provided_validator_identifiers
+    }
