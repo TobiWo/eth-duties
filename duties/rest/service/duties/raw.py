5b1622bd552684d610671d5d4b7b63946ce240ed
TobiWo <Emailkeeper@gmx.de>
2023-08-03 13:10:40 +0200

    refactor(general): Refactor cli help and method names

--- duties/rest/service/duties/raw.py
+++ duties/rest/service/duties/raw.py
@@ -5,9 +5,9 @@ from typing import List
 
 from fetcher.data_types import ValidatorDuty
 from fetcher.fetch import (
-    get_next_attestation_duties,
-    get_next_proposing_duties,
-    get_next_sync_committee_duties,
+    fetch_upcoming_attestation_duties,
+    fetch_upcoming_proposing_duties,
+    fetch_upcoming_sync_committee_duties,
 )
 
 
@@ -17,7 +17,7 @@ async def fetch_raw_attestation_duties() -> List[ValidatorDuty]:
     Returns:
         List[ValidatorDuty]: The upcoming attestation duties
     """
-    upcoming_attestation_duties = await get_next_attestation_duties()
+    upcoming_attestation_duties = await fetch_upcoming_attestation_duties()
     return list(upcoming_attestation_duties.values())
 
 
@@ -27,7 +27,7 @@ async def fetch_raw_sync_committeen_duties() -> List[ValidatorDuty]:
     Returns:
         List[ValidatorDuty]: The upcoming sync-committee duties
     """
-    upcoming_sync_committee_duties = await get_next_sync_committee_duties()
+    upcoming_sync_committee_duties = await fetch_upcoming_sync_committee_duties()
     return list(upcoming_sync_committee_duties.values())
 
 
@@ -37,5 +37,5 @@ async def fetch_raw_proposing_duties() -> List[ValidatorDuty]:
     Returns:
         List[ValidatorDuty]: The upcoming block proposing duties
     """
-    upcoming_proposing_duties = await get_next_proposing_duties()
+    upcoming_proposing_duties = await fetch_upcoming_proposing_duties()
     return list(upcoming_proposing_duties.values())
