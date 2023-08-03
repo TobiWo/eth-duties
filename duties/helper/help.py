af0f585a9812e4e0bf64952039d772f5256687c2
TobiWo <Emailkeeper@gmx.de>
2023-08-03 10:13:38 +0200

    feat(main): Improve main module
    * Fix bug where duties did not get updated while sync-committee duty ended
    * Move helper functions to help module

--- duties/helper/help.py
+++ duties/helper/help.py
@@ -1,8 +1,113 @@
 """Helper module
 """
 
-from typing import Callable
+from multiprocessing.shared_memory import SharedMemory
+from typing import Callable, List
 
-from fetcher.data_types import ValidatorDuty
+from constants.program import ALL_VALIDATOR_IDENTIFIERS_SHARED_MEMORY_NAMES
+from fetcher.data_types import DutyType, ValidatorDuty
+from fetcher.fetch import update_validator_identifiers
+from protocol.ethereum import get_current_epoch, get_current_slot, set_time_to_duty
 
 sort_duties: Callable[[ValidatorDuty], int] = lambda duty: duty.slot
+
+
+def is_current_data_up_to_date(current_duties: List[ValidatorDuty]) -> bool:
+    """Checks if the current fetched validator duties are outdated.
+
+    Args:
+        current_duties (List[ValidatorDuty]): Current validator duties fetched during last interval
+
+    Returns:
+        bool: True if current data is outdated
+    """
+    if __has_updated_validator_identifiers():
+        return False
+    if current_duties:
+        duties_up_to_date = [
+            __is_first_sync_committee_duty_up_to_date(current_duties),
+            __is_first_non_sync_committee_duty_up_to_date(current_duties),
+        ]
+        if all(duties_up_to_date):
+            return True
+    return False
+
+
+def __is_first_non_sync_committee_duty_up_to_date(duties: List[ValidatorDuty]) -> bool:
+    """Checks whether the data in memory of the first non sync-committee duty is up to date
+
+    Args:
+        duties (List[ValidatorDuty]): Current fetched duties
+
+    Returns:
+        bool: Data of first non sync-committee is up to date
+    """
+    current_slot = get_current_slot()
+    first_non_sync_committee_duty = next(
+        filter(lambda duty: duty.type is not DutyType.SYNC_COMMITTEE, duties),
+        None,
+    )
+    if (
+        first_non_sync_committee_duty
+        and first_non_sync_committee_duty.slot > current_slot
+    ):
+        return True
+    return False
+
+
+def __is_first_sync_committee_duty_up_to_date(
+    duties: List[ValidatorDuty],
+) -> bool:
+    """Checks whether the data in memory of the first sync-committee duty is up to date
+
+    Args:
+        duties (List[ValidatorDuty]): Current fetched duties
+
+    Returns:
+        bool: Data of first sync-committee is up to date
+    """
+    first_duty = duties[0]
+    if first_duty.type == DutyType.SYNC_COMMITTEE:
+        current_epoch = get_current_epoch()
+        if first_duty.epoch >= current_epoch:
+            return True
+    return False
+
+
+def __has_updated_validator_identifiers() -> bool:
+    """Checks shared memory instances for updates
+
+    Returns:
+        bool: Whether or not shared memory objects got updated
+    """
+    try:
+        identifiers_got_updated = SharedMemory("updated", False)
+        update_validator_identifiers()
+        identifiers_got_updated.close()
+        identifiers_got_updated.unlink()
+        return True
+    except FileNotFoundError:
+        pass
+    return False
+
+
+def update_time_to_duty(duties: List[ValidatorDuty]) -> None:
+    """Updates the time to duty field via call by reference
+
+    Args:
+        duties (List[ValidatorDuty]): Upcoming validator duties
+    """
+    for duty in duties:
+        set_time_to_duty(duty)
+
+
+def clean_shared_memory() -> None:
+    """Releases shared memory instances"""
+    for (
+        validator_identifier_shared_memory_name
+    ) in ALL_VALIDATOR_IDENTIFIERS_SHARED_MEMORY_NAMES:
+        shared_memory_validator_identifiers = SharedMemory(
+            validator_identifier_shared_memory_name, False
+        )
+        shared_memory_validator_identifiers.close()
+        shared_memory_validator_identifiers.unlink()
--- duties/main.py
+++ duties/main.py
@@ -4,21 +4,17 @@
 from asyncio import CancelledError, run, sleep
 from logging import getLogger
 from math import floor
-from multiprocessing.shared_memory import SharedMemory
 from platform import system
 from typing import List
 
 from cli.arguments import ARGUMENTS
 from cli.types import Mode
-from constants import logging, program
+from constants import logging
 from fetcher import fetch
-from fetcher.data_types import DutyType, ValidatorDuty
-from fetcher.fetch import update_validator_identifiers
+from fetcher.data_types import ValidatorDuty
 from fetcher.log import log_time_to_next_duties
-from helper.help import sort_duties
+from helper import help
 from helper.terminate import GracefulTerminator
-from protocol import ethereum
-from protocol.ethereum import get_current_slot
 from rest.app import start_rest_server
 
 
@@ -33,7 +29,7 @@ async def __fetch_validator_duties(
     Returns:
         List[ValidatorDuty]: Sorted list with all upcoming validator duties
     """
-    if not await __is_current_data_outdated(duties):
+    if help.is_current_data_up_to_date(duties):
         return duties
     next_attestation_duties = await fetch.get_next_attestation_duties()
     next_sync_committee_duties = await fetch.get_next_sync_committee_duties()
@@ -47,72 +43,8 @@ async def __fetch_validator_duties(
         ]
         for duty in duties.values()
     ]
-    duties.sort(key=sort_duties)
+    duties.sort(key=help.sort_duties)
     return duties
-
-
-async def __is_current_data_outdated(current_duties: List[ValidatorDuty]) -> bool:
-    """Checks if the current fetched validator duties are outdated.
-
-    Args:
-        current_duties (List[ValidatorDuty]): Current validator duties fetched during last interval
-
-    Returns:
-        bool: True if current data is outdated
-    """
-    current_slot = get_current_slot()
-    first_non_sync_committee_duty = next(
-        filter(lambda duty: duty.type is not DutyType.SYNC_COMMITTEE, current_duties),
-        None,
-    )
-    if __has_updated_validator_identifiers():
-        return True
-    if (
-        current_duties
-        and first_non_sync_committee_duty
-        and first_non_sync_committee_duty.slot > current_slot
-    ):
-        return False
-    return True
-
-
-def __has_updated_validator_identifiers() -> bool:
-    """Checks shared memory instances for updates
-
-    Returns:
-        bool: Whether or not shared memory objects got updated
-    """
-    try:
-        identifiers_got_updated = SharedMemory("updated", False)
-        update_validator_identifiers()
-        identifiers_got_updated.close()
-        identifiers_got_updated.unlink()
-        return True
-    except FileNotFoundError:
-        pass
-    return False
-
-
-def __update_time_to_duty(duties: List[ValidatorDuty]) -> None:
-    """Updates the time to duty field via call by reference
-
-    Args:
-        duties (List[ValidatorDuty]): Upcoming validator duties
-    """
-    for duty in duties:
-        ethereum.set_time_to_duty(duty)
-
-
-def __clean_shared_memory() -> None:
-    """Releases shared memory instances"""
-    for (
-        validator_identifier_shared_memory_name
-    ) in program.ALL_VALIDATOR_IDENTIFIERS_SHARED_MEMORY_NAMES:
-        shared_memory_validator_identifiers = SharedMemory(
-            validator_identifier_shared_memory_name, False
-        )
-        shared_memory_validator_identifiers.close()
-        shared_memory_validator_identifiers.unlink()
 
 
 async def main() -> None:
@@ -126,7 +58,7 @@ async def main() -> None:
     while True:
         if ARGUMENTS.mode != Mode.NO_LOG:
             upcoming_duties = await __fetch_validator_duties(upcoming_duties)
-            __update_time_to_duty(upcoming_duties)
+            help.update_time_to_duty(upcoming_duties)
             log_time_to_next_duties(upcoming_duties)
             graceful_terminator.terminate_in_cicd_mode(upcoming_duties)
             await sleep(ARGUMENTS.interval)
@@ -144,6 +76,6 @@ if __name__ == "__main__":
         else:
             run(main())
     except (CancelledError, KeyboardInterrupt) as exception:
-        __clean_shared_memory()
+        help.clean_shared_memory()
         main_logger.error(logging.SYSTEM_EXIT_MESSAGE)
     main_logger.info(logging.MAIN_EXIT_MESSAGE)
