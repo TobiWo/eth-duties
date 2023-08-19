4262a6ba0ce7c6b12055f2e42ac33d7b5b844cb8
TobiWo <Emailkeeper@gmx.de>
2023-08-19 12:11:32 +0200

    feat(rest): Improve server behavior * Add port check before starting the server * Add check if server has started

--- duties/rest/core/server.py
+++ duties/rest/core/server.py
@@ -1,9 +1,11 @@
 """Rest API server module
 """
 
+import socket as sock
 from logging import getLogger
 from multiprocessing import Process
 
+from cli.arguments import ARGUMENTS
 from constants import logging
 from uvicorn import Config as UvicornConfig
 from uvicorn import Server as UvicornServer
@@ -21,14 +23,32 @@ class RestServer(Process):
         self.server = UvicornServer(config=config)
         self.config = config
         self.logger = getLogger()
+        self.started = False
 
     def stop(self) -> None:
         """Stops the rest server programatically"""
         self.terminate()
 
     def run(self) -> None:
-        """Starts the rest server programatically"""
-        self.logger.info(logging.START_REST_SERVER_MESSAGE, self.config.port)
-        self.server.run()
-        self.logger.error(logging.SYSTEM_EXIT_MESSAGE)
-        self.logger.info(logging.MAIN_EXIT_MESSAGE)
+        """Start the rest server programatically"""
+        if not self.__is_port_in_use():
+            self.logger.info(logging.START_REST_SERVER_MESSAGE, self.config.port)
+            self.server.run()
+
+    def __is_port_in_use(self) -> bool:
+        """Check if defined port for rest server is already in use
+
+        Returns:
+            bool: Is defined port already in use
+        """
+        socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        destination = ("127.0.0.1", ARGUMENTS.rest_port)
+        connection_result = socket.connect_ex(destination)
+        if connection_result == 0:
+            self.logger.error(
+                logging.PORT_ALREADY_IN_USAGE_MESSAGE, ARGUMENTS.rest_port
+            )
+            socket.close()
+            return True
+        socket.close()
+        return False
