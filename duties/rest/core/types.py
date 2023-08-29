e1053914e0204cdf1b788cd08922537dc95c9a65
TobiWo <Emailkeeper@gmx.de>
2023-08-29 09:30:24 +0200

    feat(rest): Add rest specific DTOs

--- duties/rest/core/types.py
+++ duties/rest/core/types.py
@@ -2,6 +2,10 @@
 """
 
 from enum import Enum
+from typing import List
+
+# pylint: disable-next=no-name-in-module
+from pydantic import BaseModel
 
 
 class HttpMethod(Enum):
@@ -9,3 +13,17 @@ class HttpMethod(Enum):
 
     POST = "POST"
     DELETE = "DELETE"
+
+
+class ValidatorDuties(BaseModel):
+    """DTO for rest path /duties/any which indicates
+    whether or not there are any upcoming validator duties"""
+
+    any: bool
+
+
+class BadValidatorIdentifiers(BaseModel):
+    """DTO for rest path /validator/identifier which highlights
+    provided validators which are provided in a bad format"""
+
+    identifiers: List[str] = list("")
