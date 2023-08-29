00111ff9fe0a4e5d38ab4025d93f8395a517eacf
TobiWo <Emailkeeper@gmx.de>
2023-08-29 09:37:35 +0200

    fix(rest): Return json object from any endpoint

--- duties/rest/router/duties/any.py
+++ duties/rest/router/duties/any.py
@@ -2,16 +2,17 @@
 """
 
 from fastapi import APIRouter, status
+from rest.core.types import ValidatorDuties
 from rest.service.duties.any import any_upcoming_duties_in_queue
 
 any_duties_router = APIRouter(prefix="/duties/any", tags=["duties"])
 
 
 @any_duties_router.get("", status_code=status.HTTP_200_OK)
-async def are_upcoming_duties_in_queue() -> bool:
+async def are_upcoming_duties_in_queue() -> ValidatorDuties:
     """Check whether there are upcoming duties for the provided validators
 
     Returns:
         bool: Are there any duties in the queue for the provided validators
     """
-    return await any_upcoming_duties_in_queue()
+    return ValidatorDuties(any=await any_upcoming_duties_in_queue())
