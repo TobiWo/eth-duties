0ef7260f0f0b7ecfcde0434583d4dc7f99b430ac
TobiWo <Emailkeeper@gmx.de>
2023-09-14 16:01:18 +0200

    feat(cli): Update beacon node parsing method to parse multiple connection strings

--- duties/cli/parse.py
+++ duties/cli/parse.py
@@ -18,18 +18,27 @@ def set_validator_identifiers(validators: str) -> List[str]:
     return validators.split()
 
 
-def set_beacon_node_url(beacon_node_url: str) -> str:
-    """Parse the beacon node url
+def set_beacon_node_urls(beacon_node_urls: str) -> List[str]:
+    """Parse the beacon node urls
 
     Args:
-        beacon_node_url (str): Beacon node url
+        beacon_node_urls (str): Provided beacon node urls
 
     Raises:
-        ValueError: Error if provided url does not start with http/https
+        ValueError: Error if one or many provided urls do not start with http:// or https://
 
     Returns:
-        str: Beacon node url
+        List[str]: Parsed beacon node urls
     """
-    if not beacon_node_url.startswith("http"):
+    splitted_urls = []
+    if "," in beacon_node_urls:
+        splitted_urls = beacon_node_urls.split(",")
+        splitted_urls = [
+            url for url in splitted_urls if url.startswith(("http://", "https://"))
+        ]
+        if len(splitted_urls) != len(beacon_node_urls.split(",")):
+            raise ValueError()
+        return splitted_urls
+    if not beacon_node_urls.startswith(("http://", "https://")):
         raise ValueError()
-    return beacon_node_url
+    return [beacon_node_urls]
