2c20351818210bb5b92c94809ed2bd1da0f68993
TobiWo <Emailkeeper@gmx.de>
2023-09-15 13:30:11 +0200

    feat(rest): Add 503 response for duty endpoints
    * Add no beacon node connection dto
    * Add max wait time for fetching duties
    * Add 503 response if wait time is exceeded

--- duties/rest/router/duties/raw.py
+++ duties/rest/router/duties/raw.py
@@ -3,8 +3,9 @@
 
 from typing import List
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Response, status
 from fetcher.data_types import ValidatorDuty
+from rest.core.types import NoBeaconNodeConnection
 from rest.service.duties.raw import (
     fetch_raw_attestation_duties,
     fetch_raw_proposing_duties,
@@ -14,31 +15,49 @@ from rest.service.duties.raw import (
 raw_duties_router = APIRouter(prefix="/duties/raw", tags=["duties"])
 
 
-@raw_duties_router.get("/attestation", status_code=status.HTTP_200_OK)
-async def get_attestation_duties() -> List[ValidatorDuty]:
+@raw_duties_router.get(
+    "/attestation",
+    status_code=status.HTTP_200_OK,
+    responses={503: {"model": NoBeaconNodeConnection}},
+)
+async def get_attestation_duties(
+    response: Response,
+) -> List[ValidatorDuty] | NoBeaconNodeConnection:
     """Get upcoming attestation duties for provided validators
 
     Returns:
         List[ValidatorDuty]: The upcoming attestation duties
     """
-    return await fetch_raw_attestation_duties()
+    return await fetch_raw_attestation_duties(response)
 
 
-@raw_duties_router.get("/sync-committee", status_code=status.HTTP_200_OK)
-async def get_sync_committee_duties() -> List[ValidatorDuty]:
+@raw_duties_router.get(
+    "/sync-committee",
+    status_code=status.HTTP_200_OK,
+    responses={503: {"model": NoBeaconNodeConnection}},
+)
+async def get_sync_committee_duties(
+    response: Response,
+) -> List[ValidatorDuty] | NoBeaconNodeConnection:
     """Get upcoming sync committee duties for provided validators
 
     Returns:
         List[ValidatorDuty]: The upcoming sync committee duties
     """
-    return await fetch_raw_sync_committeen_duties()
+    return await fetch_raw_sync_committeen_duties(response)
 
 
-@raw_duties_router.get("/proposing", status_code=status.HTTP_200_OK)
-async def get_proposing_duties() -> List[ValidatorDuty]:
+@raw_duties_router.get(
+    "/proposing",
+    status_code=status.HTTP_200_OK,
+    responses={503: {"model": NoBeaconNodeConnection}},
+)
+async def get_proposing_duties(
+    response: Response,
+) -> List[ValidatorDuty] | NoBeaconNodeConnection:
     """Get upcoming block proposing duties for provided validators
 
     Returns:
         List[ValidatorDuty]: The upcoming block proposing duties
     """
-    return await fetch_raw_proposing_duties()
+    return await fetch_raw_proposing_duties(response)
--- duties/rest/service/duties/raw.py
+++ duties/rest/service/duties/raw.py
@@ -1,41 +1,73 @@
 """Service module for fetching raw validator duties
 """
 
+from asyncio import TimeoutError as AsyncioTimeoutError
+from asyncio import wait_for
 from typing import List
 
+from constants import program
+from fastapi import Response, status
 from fetcher.data_types import ValidatorDuty
 from fetcher.fetch import (
     fetch_upcoming_attestation_duties,
     fetch_upcoming_proposing_duties,
     fetch_upcoming_sync_committee_duties,
 )
+from rest.core.types import NoBeaconNodeConnection
 
 
-async def fetch_raw_attestation_duties() -> List[ValidatorDuty]:
+async def fetch_raw_attestation_duties(
+    response: Response,
+) -> List[ValidatorDuty] | NoBeaconNodeConnection:
     """Fetch upcoming attestation duties for provided validators
 
     Returns:
         List[ValidatorDuty]: The upcoming attestation duties
     """
-    upcoming_attestation_duties = await fetch_upcoming_attestation_duties()
-    return list(upcoming_attestation_duties.values())
+    try:
+        upcoming_attestation_duties = await wait_for(
+            fetch_upcoming_attestation_duties(),
+            program.REST_RAW_DUTY_NO_BEACON_NODE_CONNECTION_TIMEOUT,
+        )
+        return list(upcoming_attestation_duties.values())
+    except AsyncioTimeoutError:
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return NoBeaconNodeConnection()
 
 
-async def fetch_raw_sync_committeen_duties() -> List[ValidatorDuty]:
+async def fetch_raw_sync_committeen_duties(
+    response: Response,
+) -> List[ValidatorDuty] | NoBeaconNodeConnection:
     """Fetch upcoming sync-committee duties for provided validators
 
     Returns:
         List[ValidatorDuty]: The upcoming sync-committee duties
     """
-    upcoming_sync_committee_duties = await fetch_upcoming_sync_committee_duties()
-    return list(upcoming_sync_committee_duties.values())
+    try:
+        upcoming_sync_committee_duties = await wait_for(
+            fetch_upcoming_sync_committee_duties(),
+            program.REST_RAW_DUTY_NO_BEACON_NODE_CONNECTION_TIMEOUT,
+        )
+        return list(upcoming_sync_committee_duties.values())
+    except AsyncioTimeoutError:
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return NoBeaconNodeConnection()
 
 
-async def fetch_raw_proposing_duties() -> List[ValidatorDuty]:
+async def fetch_raw_proposing_duties(
+    response: Response,
+) -> List[ValidatorDuty] | NoBeaconNodeConnection:
     """Fetch upcoming block proposing duties for provided validators
 
     Returns:
         List[ValidatorDuty]: The upcoming block proposing duties
     """
-    upcoming_proposing_duties = await fetch_upcoming_proposing_duties()
-    return list(upcoming_proposing_duties.values())
+    try:
+        upcoming_proposing_duties = await wait_for(
+            fetch_upcoming_proposing_duties(),
+            program.REST_RAW_DUTY_NO_BEACON_NODE_CONNECTION_TIMEOUT,
+        )
+        return list(upcoming_proposing_duties.values())
+    except AsyncioTimeoutError:
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return NoBeaconNodeConnection()
