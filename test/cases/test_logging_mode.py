95f2163d26cd8f971b4782151fe7fdb06ceb6bc6
TobiWo <Emailkeeper@gmx.de>
2024-03-21 12:52:28 +0100

    test(cases): Add test cases

---
+++ test/cases/test_logging_mode.py
@@ -0,0 +1,244 @@
+"""Module with functions to test logging mode
+"""
+
+from pathlib import Path
+
+from sty import fg  # type: ignore[import]
+from test_helper.config import CONFIG, ETH_DUTIES_ENTRY_POINT
+from test_helper.functions import (
+    run_generic_test,
+    test_set_colorful_logging_thresholds,
+    test_standard_logging_mode,
+)
+from test_helper.general import get_general_eth_duties_start_command
+
+
+def test_standard_logging_mode_execution() -> int:
+    """Test for active and inactive validators supplied as index or pubkey
+    as well as for duplicates in general
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = [
+        "next ATTESTATION duty",
+        "next PROPOSING duty",
+        "is in current sync committee",
+    ]
+    validators_to_test = (
+        CONFIG.validators.active.general[0:10]
+        + CONFIG.validators.inactive
+        + CONFIG.validators.active.general[0:5]
+    )
+    command = get_general_eth_duties_start_command(
+        validators_to_test, CONFIG.general.working_beacon_node_url
+    )
+    try:
+        test_standard_logging_mode(
+            command, "duties will be executed", expected_logs, validators_to_test
+        )
+        return 1
+    except AssertionError:
+        print(fg.red + "Test Failed" + fg.rs)
+        print(
+            fg.red + "It is possible that the data "
+            "couldn't be fetched correctly from the beacon client" + fg.rs
+        )
+        return 0
+
+
+def test_standard_logging_for_supplied_pubkey() -> int:
+    """Test whether pubkeys are fetched correctly from the chain
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = ["Validator 3 has next ATTESTATION"]
+    command = get_general_eth_duties_start_command(
+        [CONFIG.validators.both_identifiers.pubkey],
+        CONFIG.general.working_beacon_node_url,
+    )
+    return run_generic_test(
+        expected_logs,
+        command,
+        "standard logging for supplied pubkey",
+        "duties will be executed",
+        additional_failure_message=(
+            "It is possible that the data couldn't be fetched correctly from the beacon client"
+        ),
+    )
+
+
+def test_pubkey_logging_mode() -> int:
+    """Test whether pubkeys are logged correctly instead of indices
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = [
+        # pylint: disable-next=line-too-long
+        "0xb99d27eeea8c7f9201926801acae031a9aa558428a47d403cfeda91260087dc77cb7e97f213b552c179d60be5d8dd671"
+    ]
+    command = get_general_eth_duties_start_command(
+        [CONFIG.validators.both_identifiers.index],
+        CONFIG.general.working_beacon_node_url,
+    ) + ["--log-pubkeys"]
+    return run_generic_test(
+        expected_logs, command, "pubkey logging mode", "duties will be executed"
+    )
+
+
+def test_alias_logging_mode() -> int:
+    """Test whether aliases are set correctly
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = ["my_index", "my_pubkey"]
+    command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.with_alias, CONFIG.general.working_beacon_node_url
+    )
+    return run_generic_test(
+        expected_logs, command, "alias logging mode", "duties will be executed"
+    )
+
+
+def test_set_logging_colors() -> int:
+    """Test whether colors for warning and critical logs are set correctly
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = ["153;255;204", "255;51;153"]
+    command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.general, CONFIG.general.working_beacon_node_url
+    ) + [
+        "--log-color-warning",
+        "153,255,204",
+        "--log-color-critical",
+        "255,51,153",
+        "--log-time-warning",
+        "480",
+        "--log-time-critical",
+        "240",
+    ]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "set logging colors",
+        "duties will be executed",
+        drop_expected_logs=True,
+        additional_failure_message=(
+            "This is most likely due to the fact that not a single validator "
+            "was in the range for the colorful warning or critical logging."
+        ),
+    )
+
+
+def test_set_colorful_logging_thresholds_execution() -> int:
+    """Test whether colorful logging thresholds are set properly
+
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    validators_to_test = CONFIG.validators.active.general
+    command = command = get_general_eth_duties_start_command(
+        validators_to_test, CONFIG.general.working_beacon_node_url
+    ) + [
+        "--log-time-warning",
+        "480",
+        "--log-time-critical",
+        "240",
+    ]
+    try:
+        test_set_colorful_logging_thresholds(command, "duties will be executed")
+        return 1
+    except AssertionError:
+        print("Test Failed")
+        return 0
+
+
+def test_logging_duties_for_high_number_of_validators() -> int:
+    """Test correct high number of validators
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = [
+        "Fetching all necessary data may take some time",
+        "Provided number of validators for fetching attestion duties is high",
+    ]
+    command = [
+        "poetry",
+        "run",
+        "python",
+        ETH_DUTIES_ENTRY_POINT,
+        "--validators-file",
+        str(Path.cwd() / "test/data/over_5k_validators"),
+        "--beacon-nodes",
+        CONFIG.general.working_beacon_node_url,
+    ]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "logging duties for high number of validators",
+        expected_logs[1],
+    )
+
+
+def test_omit_attestation_duties() -> int:
+    """Test omit attestation duties
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = [
+        "Started in mode: log",
+        "No upcoming duties detected!",
+    ]
+    command = get_general_eth_duties_start_command(
+        [CONFIG.validators.active.general[0]],
+        CONFIG.general.working_beacon_node_url,
+    ) + ["--omit-attestation-duties"]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "omit attestation duties",
+        "Logging next duties interval",
+        overhead_log_number=10,
+        additional_failure_message=(
+            "It could be that the provided validator is inactive, "
+            "in sync committee or about to propose a block!"
+        ),
+    )
+
+
+def test_increase_of_max_attestation_duty_logs() -> int:
+    """Test increase of max attestation duty logs
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = [
+        "Started in mode: log",
+        "Logging next duties interval...",
+    ]
+    command = [
+        "poetry",
+        "run",
+        "python",
+        ETH_DUTIES_ENTRY_POINT,
+        "--validators-file",
+        str(Path.cwd() / "test/data/61_validators"),
+        "--beacon-nodes",
+        CONFIG.general.working_beacon_node_url,
+        "--max-attestation-duty-logs",
+        "61",
+    ]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "increase of max attestation duty logs",
+        "all duties will be executed",
+    )
---
+++ test/cases/test_rest_api.py
@@ -0,0 +1,227 @@
+"""Module with functions to test rest api
+"""
+
+from typing import Any
+
+# pylint: disable-next=import-error
+from constants.program import REQUEST_TIMEOUT
+from requests import delete, get, post
+from test_helper.config import CONFIG
+from test_helper.functions import run_generic_test
+from test_helper.general import get_general_eth_duties_start_command
+
+
+def test_rest_while_running_in_cicd_mode() -> int:
+    """Test rest in cicd mode
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+    expected_logs = ["Rest server will not be started in any cicd-mode"]
+    command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.general[0:3], CONFIG.general.working_beacon_node_url
+    ) + [
+        "--rest",
+        "--mode",
+        "cicd-exit",
+    ]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "rest while running in cicd mode",
+        "Rest server will not be started in any cicd-mode",
+    )
+
+
+def test_get_attestation_duties_from_rest_endpoint() -> int:
+    """Test rest api get attestion duties endpoint
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+
+    def get_attestation_duties_rest_call() -> Any:
+        """Get attestation duties rest call
+
+        Returns:
+            Any: Rest call response
+        """
+        return get(
+            "http://localhost:5000/duties/raw/attestation", timeout=REQUEST_TIMEOUT
+        )
+
+    expected_logs = [
+        "Validator 1 has next ATTESTATION duty",
+        "Validator 2 has next ATTESTATION duty",
+        "Validator 3 has next ATTESTATION duty",
+    ]
+    command = command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.general[0:3], CONFIG.general.working_beacon_node_url
+    ) + ["--rest"]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "get attestation duties from rest endpoint",
+        "GET /duties/raw/attestation",
+        rest_call=get_attestation_duties_rest_call,
+        rest_call_trigger_log="all duties will be executed in",
+    )
+
+
+def test_get_sync_committee_duties_from_rest_endpoint() -> int:
+    """Test rest api get sync committee duties endpoint
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+
+    def get_sync_committee_duties_rest_call() -> Any:
+        """Get sync committee duties rest call
+
+        Returns:
+            Any: Rest call response
+        """
+        return get(
+            "http://localhost:5000/duties/raw/sync-committee", timeout=REQUEST_TIMEOUT
+        )
+
+    expected_logs = [
+        f"Validator {validator} is in current sync committee"
+        for validator in CONFIG.validators.active.in_sync_committee
+    ] + [
+        f"Validator {validator} will be in next sync committee"
+        for validator in CONFIG.validators.active.next_sync_committee
+    ]
+    command = command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.in_sync_committee
+        + CONFIG.validators.active.next_sync_committee,
+        CONFIG.general.working_beacon_node_url,
+    ) + ["--rest"]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "get sync committee duties from rest endpoint",
+        "GET /duties/raw/sync-committee",
+        rest_call=get_sync_committee_duties_rest_call,
+        rest_call_trigger_log="all duties will be executed in",
+        additional_failure_message=(
+            "Please check if provided validators still have sync committee duties in the queue."
+        ),
+    )
+
+
+def test_get_block_proposing_duties_from_rest_endpoint() -> int:
+    """Test rest api get block proposing duties endpoint
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+
+    def get_block_proposing_duties_rest_call() -> Any:
+        """Get block proposing duties rest call
+
+        Returns:
+            Any: Rest call response
+        """
+        return get(
+            "http://localhost:5000/duties/raw/proposing", timeout=REQUEST_TIMEOUT
+        )
+
+    expected_logs = [
+        f"Validator {validator} has next PROPOSING duty in"
+        for validator in CONFIG.validators.active.proposing_blocks
+    ]
+    command = command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.proposing_blocks,
+        CONFIG.general.working_beacon_node_url,
+    ) + ["--rest"]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "get block proposing duties from rest endpoint",
+        "GET /duties/raw/proposing",
+        rest_call=get_block_proposing_duties_rest_call,
+        rest_call_trigger_log="all duties will be executed in",
+        additional_failure_message=(
+            "Please check if provided validators still have proposing duties in the queue. "
+            "The recommendation is to get farthest proposing duty via beacon node call to "
+            "'http://localhost:5051/eth/v1/validator/duties/proposer/<CURRENT_EPOCH>+1'"
+        ),
+    )
+
+
+def test_post_new_validator_identifier_rest_endpoint() -> int:
+    """Test rest api add validator index endpoint
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+
+    def post_validator_identifier_rest_call() -> Any:
+        """Post validator identifier rest call
+
+        Returns:
+            Any: Rest call response
+        """
+        return post(
+            "http://localhost:5000/validator/identifier",
+            data='["3", "4", "5"]',
+            headers={"Content-type": "application/json", "Accept": "application/json"},
+            timeout=REQUEST_TIMEOUT,
+        )
+
+    expected_logs = ["POST validator identifiers: ['3', '4', '5']"] + [
+        f"Validator {validator} has next ATTESTATION duty in"
+        for validator in CONFIG.validators.active.general[0:5]
+    ]
+    command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.general[0:3],
+        CONFIG.general.working_beacon_node_url,
+    ) + ["--rest"]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "post new validator identifiers via rest endpoint",
+        "Validator 5 has next ATTESTATION duty in",
+        drop_expected_logs=True,
+        rest_call=post_validator_identifier_rest_call,
+        rest_call_trigger_log="all duties will be executed in",
+        test_rest_response_length=False,
+        overhead_log_number=10,
+    )
+
+
+def test_delete_validator_identifier_rest_endpoint() -> int:
+    """Test rest api delete validator index endpoint
+
+    Returns:
+        int: Whether or not test succeeds while 1 is success and 0 is failure
+    """
+
+    def delete_validator_identifier_rest_call() -> Any:
+        """Delete validator identifier rest call
+
+        Returns:
+            Any: Rest call response
+        """
+        return delete(
+            "http://localhost:5000/validator/identifier",
+            data='["4", "5"]',
+            headers={"Content-type": "application/json", "Accept": "application/json"},
+            timeout=REQUEST_TIMEOUT,
+        )
+
+    expected_logs = ["DELETE validator identifiers: ['4', '5']"]
+    command = get_general_eth_duties_start_command(
+        CONFIG.validators.active.general[0:5],
+        CONFIG.general.working_beacon_node_url,
+    ) + ["--rest"]
+    return run_generic_test(
+        expected_logs,
+        command,
+        "delete validator identifiers via rest endpoint",
+        "DELETE validator identifiers",
+        rest_call=delete_validator_identifier_rest_call,
+        rest_call_trigger_log="Logging next duties interval",
+        test_rest_response_length=False,
+    )
