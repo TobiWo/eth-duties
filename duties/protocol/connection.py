1a02d264329e25589ad80fb8e8480c25c5ef95e1
Tobias Wohland <Emailkeeper@gmx.de>
2024-05-16 10:23:47 +0200

    refactor(protocol): Refactor beacon node connection class for new property class

--- duties/protocol/connection.py
+++ duties/protocol/connection.py
@@ -3,8 +3,10 @@
 
 from datetime import datetime, timedelta, timezone
 from logging import getLogger
+from typing import List
 
 from cli.arguments import ARGUMENTS
+from cli.types import NodeConnectionProperties
 from constants import endpoints, logging, program
 from requests import ConnectionError as RequestsConnectionError
 from requests import ReadTimeout, Response, get
@@ -25,7 +27,9 @@ class BeaconNode:
         self.__last_used_beacon_node = ARGUMENTS.beacon_nodes[0]
         self.__logger = getLogger()
 
-    def get_healthy_beacon_node(self, is_used_beacon_node_logged: bool) -> str:
+    def get_healthy_beacon_node(
+        self, is_used_beacon_node_logged: bool
+    ) -> NodeConnectionProperties:
         """Check provided beacon nodes for availability and logs respective events
 
         Returns:
@@ -35,17 +39,17 @@ class BeaconNode:
         for index, node in enumerate(ARGUMENTS.beacon_nodes):
             if self.__is_node_healthy(node):
                 self.__log_used_beacon_node(
-                    current_time, node, is_used_beacon_node_logged
+                    current_time, node.url, is_used_beacon_node_logged
                 )
                 return node
             if ARGUMENTS.beacon_nodes[index] == ARGUMENTS.beacon_nodes[-1]:
                 self.is_any_node_healthy = False
                 self.__logger.error(logging.NO_AVAILABLE_BEACON_NODE_MESSAGE)
-            self.__log_primary_node_is_down(current_time, node)
+            self.__log_primary_node_is_down(current_time, node.url)
             self.__last_beacon_node_call_error = current_time
         return ARGUMENTS.beacon_nodes[0]
 
-    def __is_node_healthy(self, node: str) -> bool:
+    def __is_node_healthy(self, node_url: str) -> bool:
         """Check if node is healthy
 
         Args:
@@ -56,7 +60,7 @@ class BeaconNode:
         """
         try:
             response: Response = get(
-                url=f"{node}{endpoints.NODE_HEALTH_ENDPOINT}",
+                url=f"{node_url}{endpoints.NODE_HEALTH_ENDPOINT}",
                 timeout=program.REQUEST_TIMEOUT,
             )
             if response.status_code == 200:
@@ -66,7 +70,7 @@ class BeaconNode:
             return False
 
     def __log_used_beacon_node(
-        self, time: datetime, node: str, is_logged: bool
+        self, time: datetime, node_url: str, is_logged: bool
     ) -> None:
         """Log the used beacon node in dependence of specific events
 
@@ -81,14 +85,14 @@ class BeaconNode:
                 minutes=program.MINUTES_UNTIL_USED_BEACON_NODE_CONNECTION_STRING_IS_LOGGED_AGAIN
             )
             or not self.is_any_node_healthy
-            or node != self.__last_used_beacon_node
+            or node_url != self.__last_used_beacon_node
         ) and is_logged:
-            self.__logger.info(logging.USED_BEACON_NODE_MESSAGE, node)
+            self.__logger.info(logging.USED_BEACON_NODE_MESSAGE, node_url)
             self.__last_used_beacon_node_info = time
             self.is_any_node_healthy = True
-            self.__last_used_beacon_node = node
+            self.__last_used_beacon_node = node_url
 
-    def __log_primary_node_is_down(self, time: datetime, node: str) -> None:
+    def __log_primary_node_is_down(self, time: datetime, node_url: str) -> None:
         """Log if beacon node is down
 
         Args:
@@ -98,7 +102,9 @@ class BeaconNode:
         if time > self.__last_beacon_node_call_error + timedelta(
             seconds=program.SECONDS_UNTIL_BEACON_NODE_CALL_ERROR_IS_LOGGED_AGAIN
         ):
-            if node == ARGUMENTS.beacon_nodes[0]:
-                self.__logger.warning(logging.PRIMARY_BEACON_NODE_DOWN_MESSAGE, node)
+            if node_url == ARGUMENTS.beacon_nodes[0]:
+                self.__logger.warning(
+                    logging.PRIMARY_BEACON_NODE_DOWN_MESSAGE, node_url
+                )
             if len(ARGUMENTS.beacon_nodes) > 1:
                 self.__logger.info(logging.TRYING_BACKUP_NODES_MESSAGE)
